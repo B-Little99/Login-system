4655478681bc6310312a421f60b2239a09813983
Brandon Little <brandonlittle1999@hotmail.com>
2020-04-10 20:14:51 +0100

    Added JavaScript so if a user clicks on the skills text once it has already been added it will be removed.

--- portal.js
+++ portal.js
@@ -11,15 +11,15 @@ let divSkill = document.getElementsByClassName("divSkill");
 problemSolvingDiv.addEventListener("click", addSolvingText);
 
 function addSolvingText() {
-    let divCount = document.getElementById("divProblemSolving").childElementCount;
+    //This variable holds the number of children the div element has for its section
+    let childCount = document.getElementById("divProblemSolving").childElementCount; 
 
-    if (divCount === 1) {
+    if (childCount === 1) { //If there is only one child (the H3 element) it will add the text.
         let problemSolvingP = document.createElement("p");
         problemSolvingP.classList = "skillsPara";
         problemSolvingP.innerText = "Being able to quality check my work for errors was an important element to my role, ensuring I continually improved what I produced. A recent example is where a news article had a visual fault in the page. To fix the issue I looked at the HTML and CSS code, compared it to other code and researched to fix the issue.";
         problemSolvingDiv.appendChild(problemSolvingP);
-        divSkill.height = "320";
-    } else if (divCount > 1) {
+    } else if (childCount > 1) {
         let lastPSchild = problemSolvingDiv.lastChild;
         problemSolvingDiv.removeChild(lastPSchild);
     }
@@ -30,14 +30,14 @@ commsDiv.addEventListener("click", addCommsText);
 
 function addCommsText() {
 
-    let DivCount = document.getElementById("divComms").childElementCount;
+    let childCount = document.getElementById("divComms").childElementCount;
 
-    if (DivCount === 1) {
+    if (childCount === 1) {
         let commsP = document.createElement("p");
         commsP.classList = "skillsPara";
         commsP.innerText = "As the primary news coordinator, I was responsible for contacting stakeholders with different levels of seniority in the business to source articles. This required different communication mediums, such as phone, face to face and email. During significant projects I needed to effectively update my team on my progress, which involved being clear and concise on what I had achieved.";
         commsDiv.appendChild(commsP);
-    } else if (DivCount > 1) {
+    } else if (childCount > 1) {
         let lastDivChild = commsDiv.lastChild;
         commsDiv.removeChild(lastDivChild);
     }
@@ -48,14 +48,14 @@ programmingDiv.addEventListener("click", addProgrammingText);
 
 function addProgrammingText() {
 
-    let DivCount = document.getElementById("divProgramming").childElementCount;
+    let childCount = document.getElementById("divProgramming").childElementCount;
 
-    if (DivCount === 1) { 
+    if (childCount === 1) { 
         let programmingP = document.createElement("p");
         programmingP.classList = "skillsPara";
         programmingP.innerText = "Continuous research and work on a prototype website has provided me with a comprehensive understanding of HTML and CSS, recently applying JavaScript. I have learnt Git/GitHub and considerations for user experience (UX), accessibility and visual design has enabled me to improve quality.";
         programmingDiv.appendChild(programmingP);
-    } else if (DivCount > 1) {
+    } else if (childCount > 1) {
         let lastDivChild = programmingDiv.lastChild;
         programmingDiv.removeChild(lastDivChild);
     }
@@ -66,14 +66,14 @@ timeManageDiv.addEventListener("click", addTimeManagementText);
 
 function addTimeManagementText() {
 
-    let DivCount = document.getElementById("divTimeManagement").childElementCount;
+    let childCount = document.getElementById("divTimeManagement").childElementCount;
 
-    if (DivCount === 1) { 
+    if (childCount === 1) { 
         let timeManagementP = document.createElement("p");
         timeManagementP.classList = "skillsPara";
-        timeManagementP.innerText = "Continuous research and work on a prototype website has provided me with a comprehensive understanding of HTML and CSS, recently applying JavaScript. I have learnt Git/GitHub and considerations for user experience (UX), accessibility and visual design has enabled me to improve quality.";
+        timeManagementP.innerText = "I managed the creation of quarterly bulletins on the content management system, which could take up to a week to complete. Alongside this I had other projects and duties, such as the weekly news and event coordination. To complete everything to the highest standard I needed to prioritise my time, splitting my workload into achievable chunks that enabled me to focus my efforts and reach deadlines.";
         timeManageDiv.appendChild(timeManagementP);
-    } else if (DivCount > 1) {
+    } else if (childCount > 1) {
         let lastDivChild = timeManageDiv.lastChild;
         timeManageDiv.removeChild(lastDivChild);
     }
@@ -84,14 +84,14 @@ initiativeDiv.addEventListener("click", addInitiativeText);
 
 function addInitiativeText() {
 
-    let DivCount = document.getElementById("divInitiative").childElementCount;
+    let childCount = document.getElementById("divInitiative").childElementCount;
 
-    if (DivCount === 1) { 
+    if (childCount === 1) { 
         let initiativeP = document.createElement("p");
         initiativeP.classList = "skillsPara";
         initiativeP.innerText = "Oftentimes, in my previous role I worked independently as well as in a team. A good example of this is the website redesign. When the project lead was away I identified a potential bug with a component on the system. Knowing that the bug warranty was running out I made the decision to contact our agency and log the issue, as it may have fallen out of the warranty and incur further costs if left any longer.";
         initiativeDiv.appendChild(initiativeP);
-    } else if (DivCount > 1) {
+    } else if (childCount > 1) {
         let lastDivChild = initiativeDiv.lastChild;
         initiativeDiv.removeChild(lastDivChild);
     }
@@ -102,14 +102,14 @@ leadershipDiv.addEventListener("click", addLeadershipText);
 
 function addLeadershipText() {
 
-    let DivCount = document.getElementById("divLeadership").childElementCount;
+    let childCount = document.getElementById("divLeadership").childElementCount;
 
-    if (DivCount === 1) { 
+    if (childCount === 1) { 
         let leadershipP = document.createElement("p");
         leadershipP.classList = "skillsPara";
         leadershipP.innerText = "Oftentimes, in my previous role I worked independently as well as in a team. A good example of this is the website redesign. When the project lead was away I identified a potential bug with a component on the system. Knowing that the bug warranty was running out I made the decision to contact our agency and log the issue, as it may have fallen out of the warranty and incur further costs if left any longer.";
         leadershipDiv.appendChild(leadershipP);
-    } else if (DivCount > 1) {
+    } else if (childCount > 1) {
         let lastDivChild = leadershipDiv.lastChild;
         leadershipDiv.removeChild(lastDivChild);
     }
